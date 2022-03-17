b5afac774cb889a4d10cc9da61a74b81a0593ca7
brenermr <brenercaique0806@gmail.com>
2022-03-17 13:58:13 -0300

    Trivia

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -22,6 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
@@ -30,6 +32,11 @@ class GameOverFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
+
+        binding.tryAgainButton.setOnClickListener {
+
+        }
         return binding.root
+
     }
 }
